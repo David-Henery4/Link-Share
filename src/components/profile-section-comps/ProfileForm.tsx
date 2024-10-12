f59a7fa84ef2724a9ef064f1993ff1479820ce53
DizzlyD <david4henery00@hotmail.com>
2024-10-12 22:18:44 +0100

    form-input-setup

--- src/components/profile-section-comps/ProfileForm.tsx
+++ src/components/profile-section-comps/ProfileForm.tsx
@@ -4,6 +4,11 @@ const ProfileForm = () => {
   return (
     <form className="w-full mt-10">
       <ProfileImageUpload />
+      <div className="w-full mt-6 grid gap-3">
+        <Input id="first-name" label="First Name" />
+        <Input id="last-name" label="Last Name" />
+        <Input id="email" label="Email" />
+      </div>
     </form>
   );
 };
--- src/components/profile-section-comps/form-comps/Input.tsx
+++ src/components/profile-section-comps/form-comps/Input.tsx
@@ -5,9 +5,9 @@ interface InputTypes {
 
 const Input = ({ id, label }: InputTypes) => {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} name={id} />;
+    <div className="w-full">
+      <label className="text-xs text-darkGrey font-normal" htmlFor={id}>{label}*</label>
+      <input type="text" id={id} name={id} className="rounded-lg mt-1 px-4 py-3 border border-border bg-white outline-none text-darkGrey text-base font-normal" />
     </div>
   );
 };
